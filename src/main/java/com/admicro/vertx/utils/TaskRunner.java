d165060cc36fe32b7fc8124ff21468c05c0d0012
anhtn <anhtrannhat@admicro.vn>
2015-09-16 16:54:34 +0700

    Update task runner utils

--- src/main/java/com/admicro/vertx/utils/TaskRunner.java
+++ src/main/java/com/admicro/vertx/utils/TaskRunner.java
@@ -1,30 +1,36 @@
 package com.admicro.vertx.utils;
 
 import com.admicro.vertx.core.RunnableFuture;
+import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
+import io.vertx.core.Handler;
 
-import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TaskRunner {
 
-    public static <T> void loopParallelTasks(RunnableFuture<T> rf, int count, Future<Void> future) {
+    public static <T> void loopParallelTasks(RunnableFuture<T> rf, int count,
+                                             Handler<AsyncResult<Void>> handler) {
         if (count <= 0) {
-            future.complete();
+            handler.handle(Future.failedFuture("No tasks to run"));
             return;
         }
 
-        AtomicInteger ai = new AtomicInteger(count);
+        AtomicInteger remainTasks = new AtomicInteger(count);
+        AtomicBoolean finish = new AtomicBoolean(false);
 
-        for (int i = 0; i < ai.get(); i++) {
+        for (int i = 0; i < remainTasks.get(); i++) {
             Future<T> fut = Future.future();
             fut.setHandler(ar -> {
-                if (!future.isComplete()) {
+                if (!finish.get()) {
                     if (ar.failed()) {
-                        future.fail(ar.cause());
+                        finish.set(true);
+                        handler.handle(Future.failedFuture(ar.cause()));
                     } else {
-                        if (ai.decrementAndGet() == 0) {
-                            future.complete();
+                        if (remainTasks.decrementAndGet() == 0) {
+                            finish.set(true);
+                            handler.handle(Future.succeededFuture());
                         }
                     }
                 }
@@ -33,49 +39,25 @@ public class TaskRunner {
         }
     }
 
-    public static <T> void loopSequenceTasks(RunnableFuture<T> rf, int count, Future<Void> future) {
+    public static <T> void loopSequenceTasks(RunnableFuture<T> rf, int count,
+                                             Handler<AsyncResult<T>> handler) {
         if (count <= 0) {
-            future.complete();
+            handler.handle(Future.failedFuture("No tasks to run"));
             return;
         }
 
         Future<T> fut = Future.future();
         fut.setHandler(ar -> {
-            if (!future.isComplete()) {
-                if (ar.failed()) {
-                    future.fail(ar.cause());
+            if (ar.failed()) {
+                handler.handle(Future.failedFuture(ar.cause()));
+            } else {
+                if (count == 1) {
+                    handler.handle(Future.succeededFuture());
                 } else {
-                    if (count == 1) future.complete();
-                    else {
-                        TaskRunner.loopSequenceTasks(rf, count - 1, future);
-                    }
+                    TaskRunner.loopSequenceTasks(rf, count - 1, handler);
                 }
             }
         });
         rf.run(fut);
-    }
-
-    public static <T> void runParallelTasks(List<RunnableFuture<T>> rfs, Future<Void> future) {
-        final int size = rfs.size();
-        if (size == 0) {
-            future.complete();
-        } else {
-            AtomicInteger ai = new AtomicInteger(size);
-            for (RunnableFuture<T> rf : rfs) {
-                Future<T> fut = Future.future();
-                fut.setHandler(ar -> {
-                    if (! future.isComplete()) {
-                        if (ar.failed()) {
-                            future.fail(ar.cause());
-                        } else {
-                            if (ai.decrementAndGet() == 0) {
-                                future.complete();
-                            }
-                        }
-                    }
-                });
-                rf.run(fut);
-            }
-        }
     }
 }
